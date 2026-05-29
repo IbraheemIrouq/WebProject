5cc5802a859dba02faa99621152f3e409c88b779
IbraheemIrouq <ibraheem3174@gmail.com>
2026-05-29 14:38:53 +0300

    Create carData.js

---
+++ data/carData.js
@@ -0,0 +1,36 @@
+const CAR_DATA = {
+  user: 
+  {
+    name: "Username",
+    email: "username@example.com",
+    phone: "0540000002"
+  },
+
+  car: 
+  {
+    brand: "Toyota",
+    model: "Corolla",
+    year: 2026,
+    licensePlate: "ABC-1234"
+  },
+
+  status: 
+  {
+    engine: "running",
+    ac: { state: "on", temperature: 27, mode: "hot" },
+    doors: "locked",
+    lights: "on",
+    trunk: "closed"
+  },
+
+  telemetry: 
+  {
+    location: "Ramat Gan",
+    distanceFromHome: 12.5,
+    speed: 65,
+    engineTemp: 88,
+    oilTemp: 75,
+    nextService: "2026-07-15",
+    fuelLevel: 72
+  }
+};
